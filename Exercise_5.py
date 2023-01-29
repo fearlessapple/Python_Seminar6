24f810ee0e066ca8122bcb7dada12ba740720f49
Stas <smaster950@gmail.com>
2023-01-29 19:20:48 +0300

    Complete Exercise_5

--- Exercise_5.py
+++ Exercise_5.py
@@ -1,1 +1,7 @@
-    
+# Напишите функцию, которая принимает список чисел и возвращает список кортежей, 
+# где первый элемент каждого кортежа - это индекс числа, а второй элемент - это само число.
+
+numbers = [int(s) for s in input("Input your numbers: ").split()]
+def index_number_tuples(numbers):
+    return list(enumerate(numbers))
+print(index_number_tuples(numbers))
